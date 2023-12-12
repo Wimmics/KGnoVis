fc25ca6b374a61b41a0fad3a8fe8b697111cedc3
HugoCarton <hugo.carton.pro@gmail.com>
2023-12-12 16:33:52 +0100

    Résultats infructueux de fin de journée #16

--- d3_graph3.js
+++ d3_graph3.js
@@ -18,7 +18,7 @@ const dataset = {
 
 const databis = [
     {category :"A", values : [10, 20, 15, 25, 30]},
-    {category :"B", values : [5, 8, 12 , 7, 10]},
+    {category :"B", values : [5, 8, 12 , 7, 9]},
     {category :"C", values :  [6, 8, 2, 4, 5]},
     {category :"D", values :  [20, 30, 10, 12, 18]},
     {category :"E", values :  [14, 16, 24, 8, 17]}
@@ -28,6 +28,11 @@ const datatris = [
     {category : "A", value1 : 10}, {category : "A", value1 : 20}, {category : "A", value1 : 15}, {category : "A", value1 : 25}, {category : "A", value1 : 30},
     {category : "B", value1 : 5}, {category : "B", value1 : 8}, {category : "B", value1 : 12}, {category : "B", value1 : 7}, {category : "B", value1 : 10}, 
     {category : "C", value1 : 6}, {category : "C", value1 : 8}, {category : "C", value1 : 2}, {category : "C", value1 : 4}, {category : "C", value1 : 5}, 
+]
+
+const datatest = [
+    {category :"A", values : [10]},
+    {category :"B", values : [5]}
 ]
 
 // créer fonction qui prend 
@@ -138,7 +143,7 @@ const instantsvg = (donnees) =>  {
 instantsvg(databis) */
 
 
-
+/*
 const svg_creator = (donnees) => {
     const width = 400
     const height = 300
@@ -175,4 +180,50 @@ console.log(group)
         )
 }  
 
-svg_creator(datatris)
+//svg_creator(datatris)
+
+*/
+
+const svg_creator = (donnees) => {
+    const width = 400
+    const height = 300
+    const taille = donnees.length
+    let exploitable = []
+    donnees.forEach(d => {
+         for (let i = 0; i < d.values.length ; i++) {
+            exploitable.push({"Category" : d.category,
+                                "Values" : {"value" : d.values[i], "parents" : d.category}
+                            })
+                        }
+                    })
+
+    const svg = d3.select("#d3_demo_3").attr("width", width).attr("height", height)
+
+    const x_scale = d3.scaleBand().domain(donnees.map(d => d.category)).range([0, width]).padding(0.1)
+    console.log(exploitable) 
+      
+    const y_scale = d3.scaleLinear().domain([0, donnees.map(d => Math.max(...d.values))]).range([height, 0]) 
+    let group = svg.selectAll("g")
+        .data(exploitable)
+        .join(
+            enter => enter.append("g")
+                            .attr("class", d => d.Category),
+            update => update,
+            exit => exit.remove()
+        )
+
+    group.selectAll("rect")
+        .data(d => d.Values)
+        .join(
+            enter => enter.append("rect")
+                          .attr("class", "bar"),
+            update => update,
+            exit => exit.remove()
+        ).attr("x", x_scale(d.parents) + (x_scale.bandwidth() / taille))
+        .attr("y", d => y_scale(d.value))
+        .attr("width", x_scale.bandwidth() / taille)
+        .attr("height", d => height - y_scale(d.value))
+
+}  
+
+svg_creator(datatest)
