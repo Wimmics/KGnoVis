9b3fd1cc1ec443e2db55d8de7a73052a473edad2
HugoCarton <hugo.carton.pro@gmail.com>
2024-01-02 12:11:26 +0100

    Création des marges et recherche sur la légende #17

--- d3_graph3.js
+++ d3_graph3.js
@@ -7,6 +7,9 @@ const databis = [
     {category :"D", values :  [20, 30, 10, 12, 18], fill : "green"},
     {category :"E", values :  [14, 16, 24, 8, 17], fill : "black"}
 ]
+
+// Si données à charger, faire une promesse pour récupérer
+
 /*
 const svg_creator = (donnees) => {
     const width = 400
@@ -65,6 +68,7 @@ const svg_creator = (donnees) => {
 svg_creator(databis)
 */
 
+/*
 const svg_creator = (donnees) => {
     const width = 400
     const height = 300
@@ -112,25 +116,91 @@ const svg_creator = (donnees) => {
         // Il faudrait pouvoir augmenter de 1 le x_scale.bandwith à chaque valeur, pas chaque catégorie.
         .attr("y", d => y_scale(d.value))
         .attr('width', "10%")
-        .attr('height', "10%") //Bien trop grand, j'dois pas faire tout le graphique => emplacements bizarres, pourquoi?
+        .attr('height', "10%") //Bien trop grand, j'dois pas faire tout le graphique => emplacements bizarres, pourquoi? => Viewbox fait plein de petites boites, mais pas des colonnes, ça gère pas tout le doc
         //.attr("width", (x_scale.bandwidth() / taille) - varPadding)
         //.attr("height", d => height - y_scale(d.value))
         /*.attr("transform", "scale(0.5)") // N'affiche aucun changement, ptet car appliqué à tout, il me faudrait l'échelle pour vérifier
         .attr('transform', `translate(${domaine/2}, ${domaine*(-1)})`) // Déplacer l'axe vers la droite
         .call(d3.axisLeft(y_scale)) //scale 0.5
-        */.attr("fill", d => d.color)
+        .attr("fill", d => d.color)
         
         console.log(x_scale.bandwidth())
         console.log(taille)
 }  
+*/
+
+// Viewbox fait plein de petites boites, mais pas des colonnes, ça gère pas tout le doc => J'abandonne la viewbox, je pense que ce n'est pas du tout ce que je cherche. Je pense que la fonction
+// transform correspond bien plus à mes besoins actuellement.
+
+const svg_creator = (donnees) => {
+    const width = 380
+    const height = 290
+    const taille = donnees.length
+    const margin = {left : 0.1, top : 0.1, bottom : 0.1, right : 0.1}
+    const varPadding = 1
+    const domaine = Math.max(...donnees.map(d => Math.max(...d.values)))
+    let exploitable = []
+    donnees.forEach(d => {
+        let object = {"Category" : d.category}
+        object["Value"] = []
+        for (let i = 0; i < d.values.length ; i++) {
+            object.Value.push({"value" : d.values[i], "parents" : d.category, "color" : d.fill, "incr" : i})
+        }
+        exploitable.push(object)       
+        })
+    const svg = d3.select("#d3_demo_3").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom)
+
+    const x_scale = d3.scaleBand().domain(donnees.map(d => d.category)).range([0, width]).padding(0.1)      
+    const y_scale = d3.scaleLinear().domain([0, domaine]).range([height, 0]) 
+
+    let group = svg.selectAll("g")
+        .data(exploitable)
+        .join(
+            enter => enter.append("g")
+                            .attr("class", d => d.Category),
+            update => update,
+            exit => exit.remove()
+        )
+    
+    /*group.append("g")
+        .attr("transform", "scale(0.5)") // N'affiche aucun changement, ptet car appliqué à tout, il me faudrait l'échelle pour vérifier
+        .attr('transform', `translate(${domaine/2}, ${domaine*(-1)})`) // Déplacer l'axe vers la droite
+        .call(d3.axisBottom(x_scale)) //scale 0.5
+*/
+
+    group.selectAll("rect")
+        .data(d => d.Value)
+        .join(
+            enter => enter.append("rect")
+                          .attr("class", "bar"),
+            update => update,
+            exit => exit.remove()
+        )
+        .attr("x", d => x_scale(d.parents) + d.incr*(x_scale.bandwidth() / taille)) // On a la taille avec x_scale(d.parents) et on se déplace à chaque catégorie, pas à chaque élément.
+        // Il faudrait pouvoir augmenter de 1 le x_scale.bandwith à chaque valeur, pas chaque catégorie.
+        .attr("y", d => y_scale(d.value))
+        .attr("width", (x_scale.bandwidth() / taille) - varPadding)
+        .attr("height", d => height - y_scale(d.value))
+        .attr("fill", d => d.color)
+
+        // Un exemple met x_scle et y_scale dans le groupe
+        
+        console.log(x_scale.bandwidth())
+        console.log(taille)
+
+
+}  
+
 
 // Afficher les catégories en dessous des colonnes
+
+svg_creator(databis)
+
 // Afficher les valeurs en ordonnées
 // Afficher la légende
 // Changer la couleur pour chaque valeur => si on fait par catégorie
 // Ajouter un tooltip (mouseover)
 
-svg_creator(databis)
 
 /*
 // Usually you have a color scale in your chart already
@@ -138,4 +208,7 @@ var color = d3.scaleOrdinal()
   .domain(keys)
   .range(d3.schemeSet2);
 
-  */
+*/
+
+
+  
