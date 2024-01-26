55ffb26c862adb42d6715f47676ab8d2ca57210d
Sensaku <barbe.arnaud@hotmail.fr>
2024-01-26 18:00:27 +0100

    Add piechart and nodelink case

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 //import * as dependencies from "./modules/modulesDependencies.js";
 import loadChartViz from "./modules/visualisationManager.js";
+import { displayCode } from "./modules/utils/utils.js";
 
 
 const endpoint = "http://54.36.123.165:8890/sparql/";
@@ -56,6 +57,59 @@ UNION { ?curated <http://www.w3.org/ns/dcat#endpointURL> ?endpointUrl . }
 }
 } GROUP BY ?endpointUrl`
 
+const query_deka3 = `SELECT DISTINCT ?endpointUrl (MAX(?raw2) AS ?properties) {
+    GRAPH ?g {
+        { ?curated <http://www.w3.org/ns/sparql-service-description#endpoint> ?endpointUrl . }
+        UNION { ?curated <http://rdfs.org/ns/void#sparqlEndpoint> ?endpointUrl . }
+        UNION { ?curated <http://www.w3.org/ns/dcat#endpointURL> ?endpointUrl . }
+        ?metadata <http://ns.inria.fr/kg/index#curated> ?curated .
+    	?curated <http://rdfs.org/ns/void#properties> ?raw2 .
+    }
+} GROUP BY ?endpointUrl`
+
+const query_4 = `PREFIX oa:     <http://www.w3.org/ns/oa#>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+PREFIX schema:  <http://schema.org/> 
+PREFIX paragraph: <http://www.zoomathia.com/>
+
+SELECT DISTINCT ?paragraph 
+	(str(?name_animal) as ?animal)
+	(str(?name_relation) as ?relationship)
+	(str(?name_anthro) as ?anthroponyme) 
+WHERE {
+  ?annotation1 a oa:Annotation; oa:hasBody ?animal;
+	oa:hasTarget [ oa:hasSource ?paragraph;
+      oa:hasSelector [ oa:exact ?mention_animal]].
+
+  ?annotation2 oa:hasBody ?relation;
+        oa:hasTarget [oa:hasSource ?paragraph;
+                      oa:hasSelector [ oa:exact ?mention_relation]].
+
+   ?annotation3 oa:hasBody ?anthro;
+        oa:hasTarget [ oa:hasSource ?paragraph;
+                       oa:hasSelector [oa:exact ?mention_anthro]].
+
+  ?animal a skos:Concept;
+       skos:prefLabel ?name_animal.
+  ?animal_collection a skos:Collection;
+       skos:prefLabel "Ancient class"@en;
+       skos:member ?animal.
+
+  ?relation skos:prefLabel ?name_relation;
+     	            skos:broader+ ?relation_generique.
+  ?relation_generique skos:prefLabel  "special relationship"@en.
+
+ ?anthro skos:prefLabel ?name_anthro.
+ ?anthro_collection skos:prefLabel ?anthro_collection_name;
+	skos:member ?anthro.
+
+  FILTER (lang(?name_animal) = "en").
+  FILTER (lang(?name_relation) = "en")
+  FILTER (lang(?name_anthro) = "en")
+  FILTER (?anthro_collection_name in ("Place"@en, "Anthroponym"@en))
+}
+ORDER BY ?paragraph`;
+
 const parameters = {
     endpoint : "http://prod-dekalog.inria.fr/sparql",
     query : query_deka,
@@ -69,6 +123,7 @@ const parameters = {
 
 }
 loadChartViz("dekalog-1", parameters);
+displayCode("#parameters-dekalog1", parameters);
 
 const parameters2 = {
     endpoint: "http://prod-dekalog.inria.fr/sparql",
@@ -95,3 +150,34 @@ const parameters2 = {
 ]
 }
 loadChartViz("dekalog-2", parameters2)
+
+const parameters3 = {
+    endpoint: "http://prod-dekalog.inria.fr/sparql",
+    query: query_deka3,
+    type: "pie",
+    title: "Piechart of number of properties per endpoint",
+    config: [
+        {
+            label: "endpointUrl",
+            value: "properties"
+        }
+    ]
+}
+loadChartViz("dekalog-3", parameters3);
+displayCode("#piechart-deka", parameters3);
+
+const parameters4 = {
+    endpoint: "http://54.36.123.165:8890/sparql",
+    query: query_4,
+    type: "graph",
+    animation: false,
+    title: "Relationship between human, place and animal",
+    config: [
+        {
+            source:"animal",
+            target:"anthroponyme",
+            label:"relationship"
+        }
+    ]
+}
+loadChartViz("zoomathia", parameters4);
