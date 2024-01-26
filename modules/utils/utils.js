73c08650e52c616cd739059dee6290789c22bbd8
Sensaku <barbe.arnaud@hotmail.fr>
2024-01-26 18:01:30 +0100

    Add a displayCode function to avoid hardcode HTML implementation of code

--- modules/utils/utils.js
+++ modules/utils/utils.js
@@ -1,6 +1,12 @@
+const displayCode = (tag, text) => {
+    document.querySelector(tag).textContent = `${JSON.stringify(text, null, "\t")}`;
+}
+
 const executeSPARQLRequest = async (endpoint, query) => {
     const url = endpoint + "?query=" + encodeURIComponent(query) + "&format=json";
     return await fetch(url).then( response => response.json()).then(data => {return data})
 }
 
 export { executeSPARQLRequest };
+export { displayCode };
+
