fa22434fa2081a8cb25e255ff5b2040666557933
HugoCarton <hugo.carton.pro@gmail.com>
2024-01-29 09:32:24 +0100

    Suppression de la variable color #18

--- d3/d3_barchart_propre.js
+++ d3/d3_barchart_propre.js
@@ -7,9 +7,7 @@ const databis = [
     {category :"Wind", values :  [20, 30, 10, 12, 18], fill : "gold"},
     {category :"Moon", values :  [14, 16, 24, 8, 17], fill : "lightblue"}
 ]
-
-const color = ["black", "crimson", "silver", "gold", "lightblue"]
-
+/*
 function handleMouseOver() {
     d3.select(this).attr({fill : "green"})
 }
@@ -17,9 +15,9 @@ function handleMouseOver() {
 function handleMouseOut() {
     d3.select(this).attr({fill : color})
 }
+*/
 
-
-const svg_creator = (donnees, couleurs) => {
+const svg_creator = (donnees) => {
 
     // Création des constantes du graphique et de ses contours
     const width = 380
@@ -75,9 +73,27 @@ const svg_creator = (donnees, couleurs) => {
         .attr("y", d => y_scale(d.value))
         .attr("width", (x_scale.bandwidth() / taille) - varPadding)
         .attr("height", d => height - y_scale(d.value) - 10)
-        .attr("fill", (d,i) => couleurs[i])
-        .on("mouseover", handleMouseOver)
+        .attr("fill", (d,i) => color[i])
+        //.on("mouseover", handleMouseOver)
         //.on("mouseout", handleMouseOut)
+
+    // Hauteur des rectangles
+
+    group.selectAll("text")
+        .data(d => d.Value)
+        .join(
+            enter => enter.append("text").text((d => d.value))
+                          .attr("class", "text"),
+            update => update,
+            exit => exit.remove()
+        )
+	    .attr("fill","black")
+        .attr("text-anchor", "start")
+        .style("font", "12px times")
+        .attr("x", d => x_scale(d.parents) + d.incr*(x_scale.bandwidth() / taille)) // On a la taille avec x_scale(d.parents) et on se déplace à chaque catégorie, pas à chaque élément.
+        // Il faudrait pouvoir augmenter de 1 le x_scale.bandwith à chaque valeur, pas chaque catégorie.
+        .attr("y", d => y_scale(d.value) - 2)
+
 
     // Légende
 
@@ -100,24 +116,8 @@ const svg_creator = (donnees, couleurs) => {
         .style("alignment-baseline", "middle")
 
 
-    // Hauteur des rectangles
-
-    group.selectAll("text")
-        .data(d => d.Value)
-        .join(
-            enter => enter.append("text").text((d => d.value))
-                          .attr("class", "text"),
-            update => update,
-            exit => exit.remove()
-        )
-	    .attr("fill","black")
-        .attr("text-anchor", "start")
-        .style("font", "12px times")
-        .attr("x", d => x_scale(d.parents) + d.incr*(x_scale.bandwidth() / taille)) // On a la taille avec x_scale(d.parents) et on se déplace à chaque catégorie, pas à chaque élément.
-        // Il faudrait pouvoir augmenter de 1 le x_scale.bandwith à chaque valeur, pas chaque catégorie.
-        .attr("y", d => y_scale(d.value) - 2)
-        .attr("fill", d => "black")
-
+    // Tests mouseon/mouseover
+/*
     var tooltip = d3.select("body")
         .append("div")
         .style("position", "absolute")
@@ -135,9 +135,9 @@ const svg_creator = (donnees, couleurs) => {
         .on("mouseover", function(d){tooltip.text(d); return tooltip.style("visibility", "visible")})
         .on("mousemove", function(){return tooltip.style("top", (d3.event.pageX-10)+"px").style("left",(d3.event.pageY+10)+"px")})
         .on("mouseout", function(){return tooltip.style("visibility", "hidden")})
-
+*/
 }
 
-svg_creator(databis, color)
+svg_creator(databis)
 
 // Prochaine tâche : mouseover
