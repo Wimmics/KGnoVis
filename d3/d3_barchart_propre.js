1dfa9eec74c5cc90faf5250d7690e0971091de4c
HugoCarton <hugo.carton.pro@gmail.com>
2024-01-31 16:59:31 +0100

    Réflexion pour adapter au dataset #16

--- d3/d3_barchart_propre.js
+++ d3/d3_barchart_propre.js
@@ -1,11 +1,16 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm"
 
 const databis = [
-    {category :"Clouds", values : [5, 8, 12 , 7, 9], fill : "black"},
-    {category :"Flower", values : [10, 20, 15, 25, 30], fill : "crimson"},
-    {category :"Snow", values :  [6, 8, 2, 4, 5], fill : "silver"},
-    {category :"Wind", values :  [20, 30, 10, 12, 18], fill : "gold"},
-    {category :"Moon", values :  [14, 16, 24, 8, 17], fill : "lightblue"}
+    {label :"Clouds", values : [5, 8, 12 , 7, 9], fill : "black"},
+    {label :"Flower", values : [10, 20, 15, 25, 30], fill : "crimson"},
+    {label :"Snow", values :  [6, 8, 2, 4, 5], fill : "silver"},
+    {label :"Wind", values :  [20, 30, 10, 12, 18], fill : "gold"},
+    {label :"Moon", values :  [14, 16, 24, 8, 17], fill : "lightblue"}
+]
+
+const data_test = [
+    {label :"Clouds", values : [5, 8, 12 , 7, 9], fill : "black"},
+    {label :"Flower", values : [10, 20, 15, 25, 30], fill : "crimson"}
 ]
 
 const color = ["black", "crimson", "silver", "gold", "steelblue"]
@@ -17,13 +22,13 @@ const svg_creator = (donnees, couleurs = [0], vertical_bar = true, is_log = fals
 
     // Création des constantes du graphique et de ses contours
     const longueur = 380
-    const taille = donnees.length
+    const taille = Math.max(donnees.length, 5)
     const margin = {left : 5, top : 5, bottom : 5, right : 5}
     const varPadding = 1
     const domaine = Math.max(...donnees.map(d => Math.max(...d.values)*1.05))
 
     const svg = d3.select("#d3_demo_3").attr("width", longueur + margin.left + margin.right).attr("height", longueur + margin.top + margin.bottom)
-    const x_scale = d3.scaleBand().domain(donnees.map(d => d.category)).range([0, longueur]).padding(0.1)  
+    const x_scale = d3.scaleBand().domain(donnees.map(d => d.label)).range([0, longueur]).padding(0.1) 
     let y_scale   
     try {
 
@@ -42,15 +47,15 @@ const svg_creator = (donnees, couleurs = [0], vertical_bar = true, is_log = fals
     try {
 
         donnees.forEach(d => {
-            let object = {"Category" : d.category}
+            let object = {"Label" : d.label}
             object["Value"] = []
 
             for (let i = 0; i < d.values.length ; i++) {
-                object.Value.push({"value" : d.values[i], "parents" : d.category, "color" : d.fill, "incr" : i})
+                object.Value.push({"value" : d.values[i], "parents" : d.label, "color" : d.fill, "incr" : i})
             }
 
             exploitable.push(object)
-            keys.push(object.Category)
+            keys.push(object.Label)
 
             if (couleurs[0] === 0) {
                 let couleurs = {"color" : d.fill}
@@ -65,6 +70,8 @@ const svg_creator = (donnees, couleurs = [0], vertical_bar = true, is_log = fals
 
     let echelle_couleurs = d3.scaleOrdinal().domain(keys).range(color)
 
+
+
     let group = svg.selectAll("g")
         .data(exploitable)
         .join(
@@ -78,6 +85,20 @@ const svg_creator = (donnees, couleurs = [0], vertical_bar = true, is_log = fals
     // mais le problème c'est qu'à ce niveau-là je suis déjà sur mes valeurs et mes groupes sont faits.
 
     // Création des rectangles
+
+    console.log(exploitable)
+    console.log(exploitable[0].Label)
+    console.log(x_scale(exploitable[0].Label))
+    console.log(x_scale(exploitable[1].Label))
+    console.log(x_scale.bandwidth())
+    console.log(x_scale.bandwidth()/taille)
+    
+    // L'objectif est d'augmenter de 200 à chaque barre, et de 32 à chaque catégorie.
+    // x_scale(label) = 18 et 199 => endroit où débutent leurs parties. Problème : je veux pas que le 2ème commence là
+    // Est-ce que je peux récupérer le bandwith pour chaque parent, et augmenter de 1 un nombre incr à chaque itération de rect?
+    // x_scale.bandwith doit augmenter en fonction du label.
+    // x_scale(d.parents) donne l'espacement entre chaque zone, et x_bandwith me donne la taille de chaque barre (et un +1 à chaque barre)
+            
 
     if (vertical_bar == true) {
         group.selectAll("rect")
@@ -160,5 +181,5 @@ const svg_creator = (donnees, couleurs = [0], vertical_bar = true, is_log = fals
 
 }
 
-svg_creator(databis, color, true, true)
+svg_creator(data_test, undefined, true, true)
 
