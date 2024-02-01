ddaf3651ea204199eb726f66fd683ff178101a9a
Sensaku <barbe.arnaud@hotmail.fr>
2024-02-01 11:55:33 +0100

    Add example of simple visualisation for barchart

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-//import * as dependencies from "./modules/modulesDependencies.js";
 import loadChartViz from "./modules/visualisationManager.js";
 import { displayCode } from "./modules/utils/utils.js";
 
@@ -28,6 +27,149 @@ FILTER (lang(?name_animal) = "en")
 FILTER (lang(?name_construction) = "en")
 }
 GROUP BY ?name_construction`;
+
+const endpoint_local = "http://localhost:8080/sparql";
+const query_ex_1 = `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+prefix ex:      <http://www.example.org/ontology#>
+
+SELECT ?teamname ?year (count(?paper) as ?nb_paper) WHERE {
+  ?paper ex:author ?person;
+          ex:year ?year.
+  ?person ex:belong ?team.
+  ?team ex:Name ?teamname.
+}
+GROUP BY ?team ?year`;
+
+const parameters_ex_1 = {
+    endpoint: endpoint_local,
+    query: query_ex_1,
+    type: 'bar',
+    title: "Number of article per team per year",
+    config : [{
+        category: "year",
+        label: "teamname",
+        value: "nb_paper"
+    }]
+};
+loadChartViz("ex-bar1", parameters_ex_1)
+
+const query_bar_2 = `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+prefix ex:      <http://www.example.org/ontology#>
+
+SELECT ?teamname ?nb_person ?nb_paper WHERE{
+  {SELECT ?teamname (count(?person) as ?nb_person) WHERE {
+    ?person ex:belong ?team.
+    ?team ex:Name ?teamname.
+  }GROUP BY ?team}
+  
+  { SELECT ?teamname (count(?paper) as ?nb_paper) WHERE {
+        ?paper ex:author ?person;
+                ex:year ?year.
+        ?person ex:belong ?team.
+        ?team ex:Name ?teamname.
+  }GROUP BY ?team }}`
+
+const parameters_bar_2 = {
+    endpoint: endpoint_local,
+    query: query_bar_2,
+    type: 'bar',
+    title: "Number of paper and number of person per team",
+    config : [{
+        category: "nb paper",
+        label: "teamname",
+        value: "nb_paper"
+        },
+        {
+            category: "nb person",
+            label: "teamname",
+            value: "nb_person"
+        }
+    ]
+};
+loadChartViz("ex-bar2", parameters_bar_2)
+
+const parameters_bar_3 = {
+    endpoint: endpoint_local,
+    query: query_bar_2,
+    type: 'bar',
+    title: "Number of paper and number of person per team",
+    config : [{
+        label: "teamname",
+        value: "nb_paper"
+    },
+    {
+        label: "teamname",
+        value: "nb_person"
+    }]
+}
+loadChartViz("ex-bar3", parameters_bar_3)
+
+const parameters_stacked_bar = {
+    endpoint: endpoint_local,
+    query: query_bar_2,
+    type: 'bar',
+    stacked: true,
+    title: "Number of paper and number of person per team",
+    config : [{
+        label: "teamname",
+        value: "nb_paper"
+    },
+    {
+        label: "teamname",
+        value: "nb_person"
+    }]
+}
+loadChartViz("stacked-bar", parameters_stacked_bar)
+
+const query_pie_1 = `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+prefix ex:      <http://www.example.org/ontology#>
+
+SELECT ?teamname (count(?person) as ?nb_person) WHERE {
+  ?person ex:belong ?team.
+  ?team ex:Name ?teamname.
+}GROUP BY ?team`
+
+const parameters_ex_pie = {
+    endpoint: endpoint_local,
+    query: query_pie_1,
+    type: 'pie',
+    title: "Number of people per team",
+    config : [{
+        label: "teamname",
+        value: "nb_person"
+    }]
+}
+
+loadChartViz("ex-pie", parameters_ex_pie)
+
+const paramters_simple_bar = {
+    endpoint: endpoint_local,
+    query: query_pie_1,
+    type: 'bar',
+    title: "Number of people per team",
+    config : [{
+        label: "teamname",
+        value: "nb_person"
+    }]
+}
+loadChartViz("simple-bar", paramters_simple_bar)
+
+const parameters_simple_bar_row = {
+    endpoint: endpoint_local,
+    query: query_pie_1,
+    type: 'bar',
+    display: "row",
+    title: "Number of people per team",
+    config : [{
+        label: "teamname",
+        value: "nb_person"
+    }]
+}
+loadChartViz("simple-bar-row", parameters_simple_bar_row)
+
 
 const query_deka = `SELECT DISTINCT ?endpoint ?sparqlNorm (COUNT(DISTINCT ?activity) AS ?count) {
 GRAPH ?g {
@@ -172,6 +314,7 @@ const parameters4 = {
     query: query_4,
     type: "graph",
     animation: false,
+    oriented: true,
     title: "Relationship between human, place and animal",
     config: [
         {
@@ -182,3 +325,26 @@ const parameters4 = {
     ]
 }
 loadChartViz("zoomathia", parameters4);
+
+const query_node_deka = `SELECT DISTINCT ?endpointUrl ?vocabulary {
+    GRAPH ?g {
+        { ?curated <http://www.w3.org/ns/sparql-service-description#endpoint> ?endpointUrl . }
+        UNION { ?curated <http://rdfs.org/ns/void#sparqlEndpoint> ?endpointUrl . }
+        UNION { ?curated <http://www.w3.org/ns/dcat#endpointURL> ?endpointUrl . }
+        ?metadata <http://ns.inria.fr/kg/index#curated> ?curated .
+        ?curated <http://rdfs.org/ns/void#vocabulary> ?vocabulary .
+    }
+      VALUES ?vocabulary { <http://www.w3.org/1999/02/22-rdf-syntax-ns#> <http://www.w3.org/2000/01/rdf-schema#> <http://www.w3.org/ns/shacl#> <http://www.w3.org/2002/07/owl#> <http://www.w3.org/2004/02/skos/core#> <http://spinrdf.org/spin#> <http://www.w3.org/2003/11/swrl#> }
+} GROUP BY ?endpointUrl`
+
+const parameters_node_deka = {
+    endpoint: "http://prod-dekalog.inria.fr/sparql",
+    query: query_node_deka,
+    type: "graph",
+    config: [{
+        source:"endpointUrl",
+        target:"vocabulary",
+        label: "use"
+    }]
+}
+loadChartViz("deka-node", parameters_node_deka)
