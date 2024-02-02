bcfea6ca88d4a22587a856d941f0bd1eac72815f
HugoCarton <hugo.carton.pro@gmail.com>
2024-02-02 11:42:37 +0100

    Mise en forme #37

--- d3/d3_piechart1.js
+++ d3/d3_piechart1.js
@@ -48,24 +48,25 @@ svg
   .style("opacity", 0.7)
 */
 
-
 var data = [2, 4, 8, 10]
-const width = 300
-const height = 300
-const margin = {left : 5, top : 5, bottom : 5, right : 5}
+const svg_creator = (donnees) => {
 
-const svg = d3.select("#d3_demo").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom)
+    const width = 300
+    const height = 300
+    const margin = {left : 5, top : 5, bottom : 5, right : 5}
 
-const radius = Math.min(width, height) / 2
+    const svg = d3.select("#d3_demo").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom)
 
-const group = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+    const radius = Math.min(width, height) / 2
 
-const color = d3.scaleOrdinal(['#4daf4a','#377eb8','#ff7f00','#984ea3','#e41a1c'])
+    const group = svg.append("g").attr("transform", "translate(" + (width + margin.left + margin.right) / 2 + "," + (height + margin.top + margin.bottom) / 2 + ")")
+
+    const color = d3.scaleOrdinal(['#4daf4a','#377eb8','#ff7f00','#984ea3','#e41a1c'])
 
     // Generate the pie
-const pie = d3.pie()
+    const pie = d3.pie()
 
-console.log(pie(data))
+    console.log(pie(data))
 
 //data
 //index
@@ -75,20 +76,24 @@ console.log(pie(data))
 //value
 
     // Generate the arcs
-const arc = d3.arc()
-    .innerRadius(0)
-    .outerRadius(radius)
+    const arc = d3.arc()
+        .innerRadius(0)
+        .outerRadius(radius)
 
     //Generate groups
-const arcs = group.selectAll("arc")
-    .data(pie(data))
-    .enter()
-    .append("g")
-    .attr("class", "arc")
+    const arcs = group.selectAll("arc")
+        .data(pie(data))
+        .enter()
+        .append("g")
+        .attr("class", "arc")
 
     //Draw arc paths
-arcs.append("path")
-    .attr("fill", function(d, i) {
-        return color(i)
-    })
-    .attr("d", arc)
+    arcs.append("path")
+        .attr("fill", function(d, i) {
+            return color(i)
+        })
+        .attr("d", arc)
+
+}
+
+svg_creator(data)
