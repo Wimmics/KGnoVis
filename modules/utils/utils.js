4f6a466bdb718188ccfc7a6eadd2b3705efdc354
Sensaku <barbe.arnaud@hotmail.fr>
2024-02-07 17:57:28 +0100

    Fix cors error on fetch request

--- modules/utils/utils.js
+++ modules/utils/utils.js
@@ -5,16 +5,17 @@ const displayCode = (tag, text) => {
 const executeSPARQLRequest = async (endpoint, query) => {
     localStorage.clear()
     const url = `${endpoint}?query=${encodeURIComponent(query)}&format=json`;
+
     let result_data = await fetch(url, {
+        mode: 'cors',
         headers: {
-            Accept: "application/sparql-results+json"
-        }  
-    }).then( 
-        response => response.json()
-        ).then(data => data)
-    return result_data
+            'Content-Type': 'text/plain',
+            'Accept': "application/sparql-results+json"
+        }
+    })
+    //console.log(JSON.stringify(await result_data.text()))
+    return await result_data.json()
 }
 
 export { executeSPARQLRequest };
 export { displayCode };
-
