da0658a2beded519357bbe0363df314c069dbdb8
HugoCarton <hugo.carton.pro@gmail.com>
2024-02-13 18:04:07 +0100

    StackedChart auto #34

--- d3/d3_stackedchart.js
+++ d3/d3_stackedchart.js
@@ -9,9 +9,21 @@ const databis = [
 ]
 
 
-const color = ["black", "crimson", "silver", "gold", "steelblue"]
+const colors = ["black", "crimson", "silver", "gold", "steelblue"]
 
 const svg_creator = (donnees, couleurs = [0], vertical_bar = true, is_log = false, longueur = 380) => {
+
+    const margin = {left : 5, top : 5, bottom : 5, right : 5}
+    let color = []
+
+    donnees.forEach(d => {
+        if (couleurs[0] === 0) {
+            let couleurs = {"color" : d.fill}
+            color.push(couleurs.color)
+        } else {
+            color = couleurs
+        }
+    })
 
     const dataset = donnees[0].values.map((_, i) => {
         let obj = {group: donnees[0].values[i].label }
@@ -21,14 +33,42 @@ const svg_creator = (donnees, couleurs = [0], vertical_bar = true, is_log = fals
         return obj
     })
 
-    console.log(dataset)
+    const item = d3.stack().keys(["Clouds", "Flower", "Snow", "Wind", "Moon"])(dataset)
 
-    const taille = Math.max(donnees.length, 5)
-    const margin = {left : 5, top : 5, bottom : 5, right : 5}
-    const varPadding = 1
-    const domaine = d3.max(donnees, d => d3.max(d.values, e => e.value))
+    const svg = d3.select("#stacked").attr("width", longueur + margin.left + margin.right).attr("height", longueur + margin.top + margin.bottom)
 
-    const svg = d3.select("#d3_demo_3").attr("width", longueur + margin.left + margin.right).attr("height", longueur + margin.top + margin.bottom)
+    const x_scale = d3.scaleBand()
+        .domain(dataset.map(d => d.group))
+        .range([0, longueur])
+        .padding(0.1)
+  
+  // Set up the y scale
+    const y_scale = d3.scaleLinear()
+        .domain([0, d3.max(dataset, d => d.Clouds + d.Flower + d.Snow + d.Wind + d.Moon)])
+        .range([longueur, 0])
+
+    svg.append("g")
+    .selectAll("g")
+    .data(item)
+    .enter().append("g")
+        .attr("fill", (d,i) => color[i])
+    .selectAll("rect")
+    .data(d => d)
+    .enter().append("rect")
+        .attr("x", d => x_scale(d.data.group))
+        .attr("y", d => y_scale(d[1]))
+        .attr("height", d => y_scale(d[0]) - y_scale(d[1]))
+        .attr("width", x_scale.bandwidth())
+
+    // Add axes
+    svg.append("g")
+    .attr("transform", `translate(0,${longueur})`)
+    .call(d3.axisBottom(x_scale))
+
+    svg.append("g")
+        .call(d3.axisLeft(y_scale))
 
 
 }
+
+svg_creator(databis, colors)
