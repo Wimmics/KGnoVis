e72ed70410655b20f0aee627347f3d88a76fee71
HugoCarton <hugo.carton.pro@gmail.com>
2024-02-14 11:09:50 +0100

    Ajout de la légende #34

--- d3/d3_stackedchart.js
+++ d3/d3_stackedchart.js
@@ -11,12 +11,19 @@ const databis = [
 
 const colors = ["black", "crimson", "silver", "gold", "steelblue"]
 
-const svg_creator = (donnees, couleurs = [0], vertical_bar = true, is_log = false, longueur = 380) => {
+const svg_creator = (donnees, couleurs = [0], vertical_bar = true, longueur = 380) => {
+
+    // Initialisation
 
     const margin = {left : 5, top : 5, bottom : 5, right : 5}
     let color = []
+    let keys = []
 
     donnees.forEach(d => {
+        
+        let object = {"Category" : d.category}
+        keys.push(object.Category)
+
         if (couleurs[0] === 0) {
             let couleurs = {"color" : d.fill}
             color.push(couleurs.color)
@@ -35,6 +42,17 @@ const svg_creator = (donnees, couleurs = [0], vertical_bar = true, is_log = fals
 
     const item = d3.stack().keys(["Clouds", "Flower", "Snow", "Wind", "Moon"])(dataset)
 
+    let uniqueLabels = []
+    donnees.forEach(item => {
+        item.values.forEach(val => {
+            if (!uniqueLabels.includes(val.label)) {
+                uniqueLabels.push(val.label)
+            }
+        })
+    })
+
+    // Création du svg
+
     const svg = d3.select("#stacked").attr("width", longueur + margin.left + margin.right).attr("height", longueur + margin.top + margin.bottom)
 
     const x_scale = d3.scaleBand()
@@ -45,30 +63,60 @@ const svg_creator = (donnees, couleurs = [0], vertical_bar = true, is_log = fals
   // Set up the y scale
     const y_scale = d3.scaleLinear()
         .domain([0, d3.max(dataset, d => d.Clouds + d.Flower + d.Snow + d.Wind + d.Moon)])
-        .range([longueur, 0])
+        .range([longueur, 40])
 
-    svg.append("g")
-    .selectAll("g")
-    .data(item)
-    .enter().append("g")
-        .attr("fill", (d,i) => color[i])
-    .selectAll("rect")
-    .data(d => d)
-    .enter().append("rect")
+    let group = svg.selectAll("g")
+        .data(item)
+        .join(
+            enter => enter.append("g")
+                    .attr("fill", (d,i) => color[i]),
+            update => update,
+            exit => exit.remove()
+        )
+
+    console.log(item)
+    console.log(donnees)
+
+    group.selectAll("rect")
+        .data(d => d)
+        .join(
+            enter => enter.append("rect")
+                    .attr("class", "stackedbar"),
+            update => update,
+            exit => exit.remove()
+        )
         .attr("x", d => x_scale(d.data.group))
-        .attr("y", d => y_scale(d[1]))
-        .attr("height", d => y_scale(d[0]) - y_scale(d[1]))
+        .attr("y", d => y_scale(d[1])-10)
+        .attr("height", d => (y_scale(d[0]) - y_scale(d[1])))
         .attr("width", x_scale.bandwidth())
 
-    // Add axes
-    svg.append("g")
-    .attr("transform", `translate(0,${longueur})`)
-    .call(d3.axisBottom(x_scale))
 
-    svg.append("g")
-        .call(d3.axisLeft(y_scale))
+        svg.selectAll("labels")
+        .data(uniqueLabels).enter()
+        .append("text")
+        .attr("x", function(d,i){ return 15 + i*75})
+        .attr("y", longueur)
+        .text(function(d){ return d})
+        .attr("text-anchor", "left")
+        .style("alignment-baseline", "middle")
 
+    svg.selectAll("categories_dots")
+        .data(keys).enter()
+        .append("circle")
+        .attr("cx", function(d,i){ return 20 + i*75})
+        .attr("cy", 15)
+        .attr("r", 7)
+        .style("fill", (d, i) => color[i])
 
+    svg.selectAll("categories")
+        .data(keys).enter()
+        .append("text")
+        .attr("x", function(d,i){ return 30 + i*75})
+        .attr("y", 15)
+        .style("fill", (d, i) => color[i])
+        .text(function(d){ return d})
+        .attr("text-anchor", "left")
+        .style("alignment-baseline", "middle")
 }
 
 svg_creator(databis, colors)
