132ff005cc9b67a06ba015122222ff3138193a1a
HugoCarton <hugo.carton.pro@gmail.com>
2024-03-04 17:35:20 +0100

    test sur la récupération des données #44

---
+++ mission.js
@@ -0,0 +1,17 @@
+import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm"
+
+const test = () => {
+    console.log("Ca marche")
+}
+
+function recupererDonnees() {
+    var textAreaData = document.getElementById('monTextarea').value;
+    // Vous pouvez maintenant utiliser la variable textAreaData pour traiter ou afficher les données du textarea
+    alert("Données récupérées : " + textAreaData)
+}
+
+
+
+
+
+export default test; recupererDonnees
