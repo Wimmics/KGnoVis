2ba02b5378fc0affa595b33b2bf2f5d33f90d4ef
HugoCarton <hugo.carton.pro@gmail.com>
2024-03-05 15:19:15 +0100

    J'arrive à récupérer le texte de la textearea

--- mission.js
+++ mission.js
@@ -4,14 +4,10 @@ const test = () => {
     console.log("Ca marche")
 }
 
+
 function recupererDonnees() {
-    var textAreaData = document.getElementById('monTextarea').value;
-    // Vous pouvez maintenant utiliser la variable textAreaData pour traiter ou afficher les données du textarea
-    alert("Données récupérées : " + textAreaData)
-}
+    var texte = document.getElementById('user_text').value
+    console.log(texte)
+    }
 
-
-
-
-
-export default test; recupererDonnees
+export {test, recupererDonnees}
