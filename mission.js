89323990c5719a4512a85ec9c7a0c9c75d0dc257
HugoCarton <hugo.carton.pro@gmail.com>
2024-03-06 14:34:04 +0100

    ajout de la requête sparQL et correction du tableau #44

--- mission.js
+++ mission.js
@@ -7,10 +7,16 @@ const test = () => {
 function recupererDonnees() {
     var texte = document.getElementById('user_text').value
     console.log(texte)
+    return texte
     }
 
 function remplirTableau() {
-    var tableau = document.getElementById('monTableau')
+
+    var tableau = document.getElementById('result_table')
+
+    while (tableau.rows.length > 0) {
+        tableau.deleteRow(0);
+    }
 
     var donnees = [
         ["Donnée 1", "Donnée 2", "Donnée 3", "Donnée 4", "Donnée 5"],
@@ -25,8 +31,38 @@ function remplirTableau() {
         for (var j = 0; j < donnees[i].length; j++) {
             var cellule = ligne.insertCell(j)
             cellule.innerHTML = donnees[i][j]
-            }
         }
     }
+}
 
-export {test, recupererDonnees, remplirTableau}
+const executeSPARQLRequest = async (endpoint, query) => {
+    localStorage.clear()
+    const url = `${endpoint}?query=${encodeURIComponent(query)}&format=json`;
+
+    let result_data = await fetch(url, {
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'text/plain',
+            'Accept': "application/sparql-results+json"
+        }
+    })
+    //console.log(JSON.stringify(await result_data.text()))
+    return await result_data.json()
+}
+
+const query_deka = `SELECT DISTINCT ?endpoint ?sparqlNorm (COUNT(DISTINCT ?activity) AS ?count) {
+    GRAPH ?g {
+    { ?curated <http://www.w3.org/ns/sparql-service-description#endpoint> ?endpoint . }
+    UNION { ?curated <http://rdfs.org/ns/void#sparqlEndpoint> ?endpoint . }
+    UNION { ?curated <http://www.w3.org/ns/dcat#endpointURL> ?endpoint . }
+    ?metadata <http://ns.inria.fr/kg/index#curated> ?curated, ?dataset .
+        { ?dataset <http://www.w3.org/ns/prov#wasGeneratedBy> ?activity . }
+        UNION { ?metadata <http://www.w3.org/ns/prov#wasGeneratedBy> ?activity .}
+        FILTER(CONTAINS(str(?activity), ?sparqlNorm))
+        VALUES ?sparqlNorm { "SPARQL10" "SPARQL11" }
+    }
+    }
+    GROUP BY ?endpoint ?sparqlNorm
+    ORDER BY ?endpoint ?sparqlNorm`;
+
+export {test, recupererDonnees, remplirTableau, executeSPARQLRequest, query_deka}
