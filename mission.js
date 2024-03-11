225abffe56cb6cee1118d55a93dd5a67df0f338d
HugoCarton <hugo.carton.pro@gmail.com>
2024-03-11 17:51:18 +0100

    Possibilité de nommer les nodes et links sur le graph et création du dataset nodes #47

--- mission.js
+++ mission.js
@@ -4,7 +4,7 @@ function recupererDonnees() {
     var texte = document.getElementById('user_text').value
     return texte
     }
-
+/*
 function remplirTableau() {
 
     var tableau = document.getElementById('test')
@@ -29,7 +29,7 @@ function remplirTableau() {
         }
     }
 }
-
+*/
 const executeSPARQLRequest = async (endpoint, query) => {
     localStorage.clear()
     const url = `${endpoint}?query=${encodeURIComponent(query)}&format=json`;
@@ -76,8 +76,6 @@ const query_construct = `CONSTRUCT{ ?endpoint rdf:value ?sparqlNorm } {
     ORDER BY ?endpoint ?sparqlNorm`
 
 function recupererEtAfficherTableau(dataset) {
-
-    console.log(dataset)
     
     let tableauHtml = document.getElementById('result_table')
 
@@ -86,8 +84,6 @@ function recupererEtAfficherTableau(dataset) {
     }
 
     const l1 = dataset.head.vars
-
-    console.log(l1)
 
     let ligne1 = tableauHtml.insertRow(l1)
 
@@ -106,13 +102,11 @@ function recupererEtAfficherTableau(dataset) {
     }
 
 }
-
+/*
 function displayRDFGraph(rdfGraph) {
 
     const valeurs = rdfGraph.results.bindings
 
-    console.log(rdfGraph)
-    console.log(valeurs)
     // Loop through the RDF data to construct the display in the HTML document
     valeurs.forEach(triple => { // Pas une fonction : à tester
         const subject = triple.s.value
@@ -129,52 +123,137 @@ function displayRDFGraph(rdfGraph) {
 
 let couleurs = ["gold", "green"]
 
-function nodelink_creator(data, colors = [], strength = -200, width = 1000, height = 1000) {
+// Dataset transformation = datasetgraph.results.bindings
+
+function transformation(dataset) {
+
+    console.log(dataset)
+    const donnees = dataset.results.bindings
+
+    let noeuds = []
+
+    for (const ligne of donnees) {
+        let presence = false
+
+        for (const known of noeuds) {
+            if (noeuds[known] === ligne) {
+                presence = true
+            }
+        
+            if (presence === false) {
+                noeuds.push(known)
+            }
+        }
+    }
+
+    console.log(noeuds)
+}
+*/
+
+function nodeAlreadyExist(node, nodesList) {
+    for (let n of nodesList) {
+        if (n.id === node.id) {
+            return true
+        }
+    }
+    return false
+}
+
+function buildNodes(dataset) {
+
+    const nodes = []
+    const donnees = dataset.results.bindings
+
+    for (const row of donnees) {
+        let node1 = {
+            id : row["s"]["value"],
+            label : row["s"]["value"]
+        }
+        if (!(nodeAlreadyExist(node1, nodes))) {
+            nodes.push(node1)
+        }
+
+        let node2 = {
+            id : row["o"]["value"],
+            label : row["o"]["value"]
+        }
+        if (!(nodeAlreadyExist(node2, nodes))) {
+            nodes.push(node2)
+        }
+    }
+
+    console.log(nodes)
+
+}
+
+async function nodelink_creator(data, colors = [], strength = -400, width = 400, height = 400) {
   
-  const donnees = data
-  const margin = {top: 5, right: 5, bottom: 5, left: 5}
+    const donnees = data
+    const margin = {top: 5, right: 5, bottom: 5, left: 5}
 
-  if (colors.length != 2) {
-    colors = ["red", "steelblue"]
-  }
+    if (colors.length != 2) {
+        colors = ["red", "steelblue"]
+    }
 
-  const svg = d3.select("#nodelink_graph")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
+    const svg = d3.select("#nodelink_graph")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
 
-  const link = svg.selectAll("line")
-    .data(donnees.links)
-    .join("line")
-    .style("stroke", colors[0])
+    const link = svg.selectAll("line")
+        .data(donnees.links)
+        .join("line")
+        .style("stroke", colors[0])
 
-  const node = svg.selectAll("circle")
-    .data(donnees.nodes)
-    .join("circle")
-    .attr("r", 20)
-    .style("fill", colors[1])
+    const node = svg.selectAll("circle")
+        .data(donnees.nodes)
+        .join("circle")
+        .attr("r", 20)
+        .style("fill", colors[1])
+
+
+    /*let link_label = svg.selectAll("links")
+        .data(donnees.links)
+        .enter().append("text")
+        .text(d => d.relation)
+    */
+
+    /*let nodes_label = svg.selectAll("nodes")
+        .data(donnees.nodes)
+        .enter().append("text")
+        .text(d => d.label)
+    */
+
+    function ticked() {
+        link
+            .attr("x1", function(d) { return d.source.x })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; })
     
-  function ticked() {
-    link
-      .attr("x1", function(d) { return d.source.x; })
-      .attr("y1", function(d) { return d.source.y; })
-      .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; })
-    
-      node
-      .attr("cx", function (d) { return d.x+6; })
-      .attr("cy", function(d) { return d.y-6; })
-  }
+        node
+            .attr("cx", function (d) { return d.x+6; })
+            .attr("cy", function(d) { return d.y-6; })
 
-  const simulation = d3.forceSimulation(donnees.nodes)       
-    .force("link", d3.forceLink()                      
-      .id(function(d) { return d.id; })                
-      .links(donnees.links)                                 
-    )
-    .force("charge", d3.forceManyBody().strength(strength))
-    .force("center", d3.forceCenter(width / 2, height / 2))
-    .on("end", ticked)
+        /*link_label.attr("x", d => (d.source.x + d.target.x) / 2)
+            .attr("y", d => (d.source.y + d.target.y) / 2)
+        */
+
+        /*nodes_label.attr("x", d => d.x)
+            .attr("y", d => d.y)
+        */
+
+    }
+
+    const simulation = d3.forceSimulation(donnees.nodes)       
+        .force("link", d3.forceLink()                      
+            .id(function(d) { return d.id; })                
+            .links(donnees.links)                                 
+        )
+        .force("charge", d3.forceManyBody().strength(strength))
+        .force("center", d3.forceCenter(width / 2, height / 2))
+        .on("end", ticked)
     
 }
 
-export {recupererDonnees, remplirTableau, executeSPARQLRequest, recupererEtAfficherTableau, displayRDFGraph, nodelink_creator}
+export {recupererDonnees, executeSPARQLRequest, recupererEtAfficherTableau, nodelink_creator, nodeAlreadyExist, buildNodes}
